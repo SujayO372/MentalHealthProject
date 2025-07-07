1a7f9c4f08ede1275fdc441bc287693e45c04c59
SujayO372 <sujayoggu@gmail.com>
2025-07-06 20:27:44 -0700

    added hotlines

--- MentalHealthProject/src/pages/HealthTest.jsx
+++ MentalHealthProject/src/pages/HealthTest.jsx
@@ -1,9 +1,14 @@
 import NavBar from '../components/NavBar'
 export default function HealthTest() {
   return (
-    <>
+    <div>
       <NavBar />
-      <h1>HealthTest Page</h1>
-    </>
-  )
+      <div >
+        <h1> Various Health Tests </h1>
+        <article>
+        </article>
+      </div>
+    </div>
+  );
 }
+
--- MentalHealthProject/src/pages/Hotlines.jsx
+++ MentalHealthProject/src/pages/Hotlines.jsx
@@ -1,9 +1,36 @@
-import NavBar from '../components/NavBar'
+import NavBar from '../components/NavBar';
+
 export default function Hotlines() {
   return (
-    <>
+    <div>
       <NavBar />
-      <h1>Hotlines Page</h1>
-    </>
-  )
+      <div >
+        <h1> Emergency Hotlines for Support </h1>
+        <article>
+          <ul>
+            <li> <strong>Emergency Services:</strong> 911 </li>
+            <li><strong>National Suicide Prevention Lifeline:</strong> 1-800-273-TALK (8255)</li>
+            <li><strong>Crisis Text Line:</strong> Text HOME to 741741</li>
+            <li><strong>Domestic Violence Hotline:</strong> 1-800-799-SAFE (7233)</li>
+            <li><strong>Childhelp National Child Abuse Hotline:</strong> 1-800-4-A-CHILD (422-4453)</li>
+            <li><strong>National Sexual Assault Hotline (RAINN):</strong> 1-800-656-HOPE (4673)</li>
+            <li><strong>LGBT National Hotline:</strong> 1-888-843-4564</li>
+            <li><strong>Veterans Crisis Line:</strong> 1-800-273-8255 (Press 1)</li>
+            <li><strong>Elder Care Locator:</strong> 1-800-677-1116</li>
+            <li><strong>Poison Control:</strong> 1-800-222-1222</li>
+            <li><strong>National Runaway Safeline:</strong> 1-800-RUNAWAY (786-2929)</li>
+            <li><strong>Disaster Distress Helpline:</strong> 1-800-985-5990</li>
+            <li><strong>National Human Trafficking Hotline:</strong> 1-888-373-7888</li>
+            <li><strong>National Eating Disorders Association (NEDA):</strong> 1-800-931-2237</li>
+            <li><strong>National Alliance on Mental Illness (NAMI) Helpline:</strong> 1-800-950-NAMI (6264)</li>
+            <li><strong>SAMHSA Treatment Locator:</strong> 1-800-662-HELP (4357)</li>          </ul>
+          <p>  Hotlines are dedicated phone lines designed to provide immediate assistance, support, and information for specific issues. 
+            Whether it’s for mental health crises, domestic violence, substance abuse, or emergency services, hotlines offer confidential, timely help to individuals in need. 
+            They serve as a vital resource for connecting people with the appropriate care and guidance.
+
+  </p>
+        </article>
+      </div>
+    </div>
+  );
 }
