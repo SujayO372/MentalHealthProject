4a4fc7acf92647a9dc25726e2d893d215def0907
SujayO372 <sujayoggu@gmail.com>
2025-07-08 20:04:27 -0700

    Update Chatbot.jsx

--- MentalHealthProject/src/pages/Chatbot.jsx
+++ MentalHealthProject/src/pages/Chatbot.jsx
@@ -1,10 +1,51 @@
-import NavBar from '../components/NavBar'
+import NavBar from '../components/NavBar';
+
+const QuestionsToAsk = [
+  {
+    id: 0,
+    question: 'How have you been feeling emotionally over the past two weeks?'
+  },
+  {
+    id: 1,
+    question: 'Have you experienced any recent changes in sleep patterns?'
+  },
+  {
+    id: 2,
+    question: 'Do you often feel overwhelmed or unable to cope with daily tasks?'
+  },
+  {
+    id: 3,
+    question: 'Have you noticed any changes in your appetite or eating habits?'
+  },
+  {
+    id: 4,
+    question: 'Do you find it difficult to concentrate or stay focused?'
+  },
+  {
+    id: 5,
+    question: 'Have you had any thoughts of self-harm or harming others?'
+  }
+];
+
+
 export default function Chatbot() {
+  const message = {
+    isUser: true,
+    messageContent: "hi how are you?",
+    "time-stamp": "dummy-data",
+  };
+  
+
   return (
     <>
       <NavBar />
       <h1>Chatbot Page</h1>
+
       
     </>
-  )
+  );
 }
+
+/*
+
+*/
