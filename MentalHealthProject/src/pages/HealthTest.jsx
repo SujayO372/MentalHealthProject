9f2fb5ed54ca420514315b86d9e79590d3ebb47b
SujayO372 <sujayoggu@gmail.com>
2025-08-14 18:39:37 -0700

    push

--- MentalHealthProject/src/pages/HealthTest.jsx
+++ MentalHealthProject/src/pages/HealthTest.jsx
@@ -73,7 +73,7 @@ export default function HealthTest() {
                 fontSize: 32,
               }}
             >
-              Mental Wellness Check-In
+              Mental Wellness Test
             </h1>
             {QuestionsToAsk.map(({ id, question }) => (
               <section
